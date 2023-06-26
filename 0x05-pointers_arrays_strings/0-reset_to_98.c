5d36089a2483d758710faa685f1bbbece593fc9a
terrassotree <elmeknassi2000@gmail.com>
2023-06-26 16:29:53 -0700

    reset_to_98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,40 @@
+#include <unistd.h>
+
+/**
+ * _putchar - Writes a character to the standard output (stdout)
+ * @c: The character to be written
+ *
+ * Return: On success, returns the character written.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * update_value - Updates the value pointed to by a given pointer to 98.
+ * @ptr: Pointer to an int to be updated
+ */
+void update_value(int *ptr)
+{
+	*ptr = 98;
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num = 0;
+	int *ptr = &num;
+
+	update_value(ptr);
+
+	_putchar(*ptr + '0');
+	_putchar('\n');
+
+	return (0);
+}
