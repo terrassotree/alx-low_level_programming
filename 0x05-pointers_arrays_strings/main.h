bb9fe94837e61ed025df776a99fb4282e3140610
terrassotree <elmeknassi2000@gmail.com>
2023-06-26 17:57:49 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -10,5 +10,6 @@ int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
-oid puts2(char *str);
+void puts2(char *str);
+void print_array(int *a, int n);
 #endif
