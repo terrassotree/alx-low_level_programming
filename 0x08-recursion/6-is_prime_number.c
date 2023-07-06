38f957eab53b2930b5b9954086a67d7f3ba22538
terrassotree <elmeknassi2000@gmail.com>
2023-07-05 18:30:56 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,39 +1,31 @@
+#include "holberton.h"
+
 /**
- * is_prime_number - Checks if a number is a prime number
- * @n: The number to be checked
+ * prime2 - Makes possible to evaluate from 1 to n
+ * @a: same number as n
+ * @b: number that iterates from 1 to n
  *
- * Description:
- * The is_prime_number function checks if a given number is a prime number.
- * It takes an integer parameter 'n' and recursively checks if 'n' is divisible
- * by any number from 2 up to the square root of 'n'.
- * If 'n' is divisible by any
- * of these numbers, it is not a prime number
- * and the function returns 0. If 'n'
- * is not divisible by any of these numbers,
- * it is a prime number and the function
- * returns 1.
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int prime2(int a, int b)
+{
+	if (a == b)
+		return (1);
+	else if (a % b == 0)
+		return (0);
+	return (prime2(a, b + 1));
+}
+/**
+ * is_prime_number - checks if a number is prime
+ * @n: Number Integer
  *
- * Return: 1 if 'n' is a prime number, 0 otherwise
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
-	return (is_prime_number(n));
-}
-
-/**
- * is_prime_recursive - Helper function to check for prime recursively
- * @n: The number to be checked
- * @divisor: The current divisor being checked
- *
- * Return: 1 if 'n' is a prime number, 0 otherwise
- */
-int is_prime_recursive(int n, int divisor)
-{
-	if (divisor * divisor > n)
-		return (1);
-	if (n % divisor == 0)
-		return (0);
-	return (is_prime_recursive(n, divisor + 1));
+	return (prime2(n, 2));
 }
